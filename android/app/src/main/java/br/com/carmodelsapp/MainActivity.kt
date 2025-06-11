9e447aad3491696ae794b93dd0de8940315d7bf8
clertonf <clerton.filho7@gmail.com>
2025-06-11 10:21:52 -0300

    rename package

--- android/app/src/main/java/br/com/carmodelsapp/MainActivity.kt
+++ android/app/src/main/java/br/com/carmodelsapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.clertonf.CarModelsApp
+package br.com.carmodelsapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/br/com/carmodelsapp/MainApplication.kt
+++ android/app/src/main/java/br/com/carmodelsapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.clertonf.CarModelsApp
+package br.com.carmodelsapp
 
 import android.app.Application
 import android.content.res.Configuration
